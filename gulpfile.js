48102cbad1542ff4e934a6c3eddb5bb1c082f1b5
pablo <par10@me.com>
2018-11-25 21:24:36 +0100

    trabajo en proceso uno

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 //VARIABLES DE PLUGINS
 var gulp = require('gulp'),
-    watch = require('gulp-watch')
-
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import')
   ;
 // DEFAULT TASK
 gulp.task('default', function() {
@@ -13,9 +17,13 @@ gulp.task('html', function() {
 });
 //estilos TASK
 gulp.task('estilos', function() {
-  console.log('task estilos para esoS STILOS');
+  return gulp.src('./app/assets/styles/styles.css')
+          // FILTERS DE POSTCSS esperando un ARRAY
+          .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+          // ultimo PIPE cond DEST crea la carpeta y el file
+          .pipe(gulp.dest('./app/temp/styles'));
 });
-// mira TASK que utiliza VARIOS WATCH(DONDE, QUE HACE (OTRA TAREA)X3)
+// mira TASK que utiliza VARIOS WATCH(DONDE, QUE  (OTRA TAREA)X3)
 gulp.task('mira', function() {
   watch('./app/index.html', function(){
     gulp.start('html');
